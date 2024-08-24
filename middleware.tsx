290d980dec8059a819aabc7a3bc2b748c97175dc
himangshu <himangshu73@gmail.com>
2024-08-24 08:19:07 +0600

    middleware

---
+++ middleware.tsx
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const publicPath = path === "/login" || path === "/signup";
+  const token = request.cookies.get("token")?.value || "";
+
+  if (publicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+  if (!publicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+}
+
+export const config = {
+  matcher: ["/", "/profile/:path*", "/login", "/signup"],
+};
